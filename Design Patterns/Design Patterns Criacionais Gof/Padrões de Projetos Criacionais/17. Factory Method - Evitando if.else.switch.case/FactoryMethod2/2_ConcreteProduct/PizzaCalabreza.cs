8fac93edd06a347f40d77dc2099b9a0e02208850
Felipe Dutra <felipe.franz.silva@hotmail.com>
2021-07-11 13:11:05 -0300

    GOf

---
+++ Design Patterns/Design Patterns Criacionais Gof/Padrões de Projetos Criacionais/17. Factory Method - Evitando if.else.switch.case/FactoryMethod2/2_ConcreteProduct/PizzaCalabreza.cs
@@ -0,0 +1,15 @@
+﻿namespace FactoryMethod2
+{
+    //ConcreteProduct
+    public class PizzaCalabreza : Pizza
+    {
+        private readonly string _nomePizza;
+        public PizzaCalabreza()
+        {
+            _nomePizza = "Pizza Calabreza";
+            Ingredientes.Add("Fatias de calabreza especial");
+            Ingredientes.Add("Queijo parmessão italiano tradicional");
+        }
+        public override string Nome { get => _nomePizza; }
+    }
+}
---
+++ Design Patterns/Design Patterns Criacionais Gof/Padrões de Projetos Criacionais/17. Factory Method - Evitando if.else.switch.case/FactoryMethod2/4_ConcreteCreator/PizzaFactory.cs
@@ -0,0 +1,26 @@
+﻿using FactoryMethod2;
+using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1.ConcreteCreator
+{
+    //Concrete Creator
+    public class PizzaFactory : PizzaFactoryMethod
+    {
+        public override Pizza CriarPizza(int tipo)
+        {
+            var factory = PizzaFactories[tipo];
+            return factory();
+        }
+
+        public static Dictionary<int, Func<Pizza>> PizzaFactories =
+                  new Dictionary<int, Func<Pizza>>
+        {
+                  { 1, ()=>new PizzaCalabreza() },
+                  { 2, ()=>new PizzaMussarela() },
+                  { 3, ()=>new PizzaPortuguesa()},
+                  { 4, ()=>new PizzaQuatroQueijos() },
+        };
+    }
+}
+
